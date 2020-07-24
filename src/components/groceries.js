3b3847e27169fac78a352cf1d9500f284e27ae19
github username <github email address>
2020-07-23 17:17:22 -0700

    move data from functions from groceries class

--- src/components/groceries.js
+++ src/components/groceries.js
@@ -1,30 +1,24 @@
 class Groceries {  
     constructor() {
-        this.groceries = []
         this.adapter = new GroceriesAdapter()
-        // this.bindEventListeners()
+        this.bindEventListeners()
         this.fetchAndLoadGroceries()
     }
+
+    bindEventListeners() {
+        this.groceriesContainer = document.getElementById('groceries-container')
+    }
+
 
     fetchAndLoadGroceries() {
         this.adapter
         .getGroceries()
         .then(groceries => {
-        //  return console.log(groceries)
-        groceries.data.forEach(note => this.groceries.push(note))
-        console.log(this.groceries)
+        groceries.data.forEach(grocery => {  
+        const newGrocery = new Grocery(grocery, grocery.attributes)
+        this.groceriesContainer.innerHTML += newGrocery.render()
         })
-        .then(() => {
-            this.render()
-        })
-    }
-    
-    render() {
-        const groceriesContainer = document.getElementById('groceries-container')
-        groceriesContainer.innerHTML = 'my notes'
-        // console.log('my groceries are', this.groceries)
-    }
-
+    })
 }
-
-// The meat of my code will on the page
+   
+}
--- src/components/grocery.js
+++ src/components/grocery.js
@@ -1,5 +1,20 @@
 class Grocery {
-    constructor(grocery, groceryJson)
-    this.id = groceryJson.id
-    this.name = groceryJson.name
+    constructor(grocery, groceryJson){ 
+        this.id = grocery.id
+        this.market = groceryJson.market
+        this.groceryItem = groceryJson.grocery_item
+        this.qty = groceryJson.qty
+        Grocery.all.push(this)
+     }
+    
+    render() {
+        return `<li grocery.id=${this.id}>${this.market.name}${this.groceryItem}</li>`
+    }
+
+
+
 }
+
+Grocery.all = []
+
+    
